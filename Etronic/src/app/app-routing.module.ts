7a18e4bb94595bdb2c8fd95434c88ec674c59bb5
W E D Y <110571114+WedyDev@users.noreply.github.com>
2023-10-22 01:59:30 -0500

    2commit

--- Etronic/src/app/app-routing.module.ts
+++ Etronic/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/Landingpage/home/home.component';
+import { AboutusComponent } from './views/Landingpage/aboutus/aboutus.component';
+import { PlayComponent } from './views/Landingpage/play/play.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home',component: HomeComponent },
+  {
+    path: '', redirectTo: 'Home', pathMatch: 'full'
+  },
+  {
+    path: 'Home',component: HomeComponent
+   },
+   {
+    path:'AboutUs',component:AboutusComponent
+   },
+   {
+    path:'Play',component:PlayComponent
+
+   }
 ];
 
 @NgModule({
